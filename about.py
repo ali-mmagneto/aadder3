e627ab122ba5453d1209b4f552c7ca14dbebe5d8
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-23 14:17:00 +0530

    Create about.py

---
+++ about.py
@@ -0,0 +1,15 @@
+ABOUT_TEXT = """
+**Bot :** `Video Subtitle Merger`
+**Creator :** [Tellybots_4u](https://telegram.me/tellybots_4u)
+**Channel :** [Tellybots_4u](https://telegram.me/tellybots_4u)
+**Credits :** `Everyone in this journey`
+**Language :** [Python3](https://python.org)
+**Library :** [Pyrogram v1.2.0](https://pyrogram.org)
+**Server :** [Heroku](https://heroku.com)
+"""
+    ABOUT_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🏠 Home', callback_data='home'),
+        InlineKeyboardButton('Close 🔐', callback_data='close')
+        ]]
+    )
