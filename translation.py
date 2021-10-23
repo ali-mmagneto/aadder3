718a432575dfdd662108999d8a34124174c95a8b
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-23 14:31:39 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -2,7 +2,7 @@
 class Translation (object):
 
     START_TEXT = """<b>Hey,</b>\n
-<b>This is a Telegram Bot to Merge into a video</b>
+<b>This is a Telegram Bot to Merge Subtitle into a video</b>
 
 <b>Send me a Telegram file to Get started</b>
 
