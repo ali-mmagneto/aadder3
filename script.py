175065311783ec2224917efb2833f80b6fc719dc
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-23 14:49:21 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,14 +1,4 @@
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 class Script(object):
-
-    START_BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('🤔 Help', callback_data='help'),
-        InlineKeyboardButton('🤖 About', callback_data='about'),
-        ],[
-        InlineKeyboardButton('Close🔐', callback_data='close')
-        ]]
-    )
-    
-    
+......
