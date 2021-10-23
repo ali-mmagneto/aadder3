213f1585983bcc763304806813fd01ca28e894f3
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-23 14:05:34 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -23,7 +23,7 @@ async def help_user(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.HELP_TEXT,
-        reply_markup=Script.HELP_BUTTON,
+        reply_markup=Script.HELP_BUTTONS,
         parse_mode="html",
         disable_web_page_preview=True,
         reply_to_message_id=update.message_id
