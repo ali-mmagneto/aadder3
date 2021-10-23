f57668e5768ded0342e6b4a30a9e0842a20c8635
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-23 14:57:21 +0530

    Update cb_buttons.py

--- plugins/cb_buttons.py
+++ plugins/cb_buttons.py
@@ -4,24 +4,24 @@ if bool(os.environ.get("WEBHOOK", False)):
     from Config import Config
 else:
     from config import Config
+from help import Help
 from about import About
 from script import Script
 from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-
 @Client.on_callback_query()
 async def cb_handler(bot, update):
     if update.data == "home":
         await update.message.edit_text(
-            text=Translation.START_TEXT.format(update.from_user.mention),
+            text=Script.START_TEXT.format(update.from_user.mention),
             reply_markup=Script.START_BUTTONS,
             disable_web_page_preview=True
         )
     elif update.data == "help":
         await update.message.edit_text(
-            text=Translation.HELP_TEXT,
-            reply_markup=Script.HELP_BUTTONS,
+            text=Help.HELP_TEXT,
+            reply_markup=Help.HELP_BUTTONS,
             disable_web_page_preview=True
         )
     elif update.data == "about":
