6e5fb8a081f3e08591f367543de603036ff6d2b8
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-23 14:43:00 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -10,19 +10,7 @@ Use help Command for more details..
 
     """
 
-    HELP_TEXT ="""<b>Welcome to the Help Menu</b>
-
-1.) Send a Video file or url.
-2.) Send a subtitle file (ass or srt)
-3.) Choose you desired type of muxing!
-
-To give custom name to file send it with url seperated with |
-<i>url|custom_name.mp4</i>
-
-<b>Note : </b><i>Please note that only english type fonts are supported in hardmux other scripts will be shown as empty blocks on the video!</i>
-
-<a href="https://github.com/sahaynitin/video-sub-merger">Repo URL</a>"""
-
+    
     
     DOWNLOAD_SUCCESS = """File downloaded successfully!
 
