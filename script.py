fc07a6caf6568f115ea37ed663607e667490ec5d
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-23 14:49:46 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,4 +1,22 @@
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 class Script(object):
-......
+
+    START_TEXT = """<b>Hey,</b>\n
+<b>This is a Telegram Bot to Merge Subtitle into a video</b>
+
+<b>Send me a Telegram file to Get started</b>
+
+Use help Command for more details..
+
+    """
+    START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🤔 Help', callback_data='help'),
+        InlineKeyboardButton('🤖 About', callback_data='about'),
+        ],[
+        InlineKeyboardButton('Close🔐', callback_data='close')
+        ]]
+    )
+    
+    
