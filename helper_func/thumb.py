f35c8c529602f3e32d649e9a7b29c5690cc4ed0e
hackertyus <88194972+hackertyus@users.noreply.github.com>
2021-10-24 23:22:43 +0300

    Create thumb.py

---
+++ helper_func/thumb.py
@@ -0,0 +1,34 @@
+import os
+import time
+import ffmpeg
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+
+def get_thumbnail(in_filename, path, ttl):
+    out_filename = os.path.join(path, str(time.time()) + ".jpg")
+    open(out_filename, 'a').close()
+    try:
+        (
+            ffmpeg
+            .input(in_filename, ss=ttl)
+            .output(out_filename, vframes=1)
+            .overwrite_output()
+            .run(capture_stdout=True, capture_stderr=True)
+        )
+        return out_filename
+    except ffmpeg.Error as e:
+      return None
+
+def get_duration(filepath):
+    metadata = extractMetadata(createParser(filepath))
+    if metadata.has("duration"):
+      return metadata.get('duration').seconds
+    else:
+      return 0
+
+def get_width_height(filepath):
+    metadata = extractMetadata(createParser(filepath))
+    if metadata.has("width") and metadata.has("height"):
+      return metadata.get("width"), metadata.get("height")
+    else:
+      return 1280, 720
