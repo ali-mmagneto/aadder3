91e308f3af807f5e0919f6320e7ec294442f1f57
Tellybots <93035329+Tellybots@users.noreply.github.com>
2021-10-24 17:56:47 +0530

    Update forcesub.py

--- plugins/forcesub.py
+++ plugins/forcesub.py
@@ -15,7 +15,7 @@ async def handle_force_subscribe(bot, message):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=message.from_user.id,
-                text="Sorry Sir, You are Banned. Contact My [Support Group](https://t.me/safothebot).",
+                text="Sorry Sir, You are Banned. Contact My [Support Group](https://t.me/tellybots_4u).",
                 parse_mode="markdown",
                 disable_web_page_preview=True,
                 reply_to_message_id=message.message_id,
