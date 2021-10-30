6d87eebf8fab27d261fad5fa3a2cb65141d233a5
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2021-10-30 12:27:03 +0300

    Update translation.py

--- translation.py
+++ translation.py
@@ -5,11 +5,11 @@ class Translation (object):
 
     
     
-    DOWNLOAD_SUCCESS = """File downloaded successfully!
+    DOWNLOAD_SUCCESS = """Dosya Başarı İle İndirildi!
 
-Time taken : {} seconds."""
-    FILE_SIZE_ERROR = "ERROR : Cannot Extract File Size from URL!"
-    MAX_FILE_SIZE = "File size is greater than 2Gb. Which is the limit imposed by telegram!"
-    LONG_CUS_FILENAME = """Filename you provided is greater than 60 characters.
-Please provide a shorter name."""
-    UNSUPPORTED_FORMAT = "ERROR : File format {} Not supported!"
+Geçen Süre : {} saniye."""
+    FILE_SIZE_ERROR = "HATA : URL'den Dosya Boyutu Ayıklanamıyor!"
+    MAX_FILE_SIZE = "Dosya boyutu 2 Gb'den büyük. Telegramın getirdiği sınır da bu!"
+    LONG_CUS_FILENAME = """Sağladığınız dosya adı 60 karakterden büyük.
+Lütfen daha kısa bir ad girin."""
+    UNSUPPORTED_FORMAT = "Hata : Dosya Şekli {} Desteklenmiyor!"
