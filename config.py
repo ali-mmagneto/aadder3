da5c3a4cedddbfb8367c1cb436bc14e957daa308
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-05-04 14:06:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
     APP_ID = os.environ.get('APP_ID', None)
     API_HASH = os.environ.get('API_HASH', None)
-    FFMPEG = os.environ.get('FFMPEG', None)
+    FFMPEG = os.environ.get("FFMPEG", "superfast")
 
     #comma seperated user id of users who are allowed to use
     #ALLOWED_USERS = [x.strip(' ') for x in os.environ.get('ALLOWED_USERS','1098504493').split(',')]
