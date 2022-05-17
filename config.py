34d7ce40e873732966712add87a2fdf38c328842
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-05-17 20:08:31 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 
 class Config:
 
+    ENCODE_DIR = environ.get("ENCODE_DIR", "encodes")
     BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
     APP_ID = os.environ.get('APP_ID', None)
     API_HASH = os.environ.get('API_HASH', None)
