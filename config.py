0127edbdcea5deb8529130e8773bb972ac14b0c1
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-05-30 20:15:20 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ import os
 class Config:
 
     ENCODE_DIR = os.environ.get("ENCODE_DIR", "encodes")
-    BOT_TOKEN = "1974636862:AAG2upXULcE2bjqQRXgk1B1XifQzTL-CP5I"
-    APP_ID = 2374174
-    API_HASH = "c23db4aa92da73ff603666812268597a"
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", none) 
+    APP_ID = os.environ.get("APP_ID", none)
+    API_HASH = os.environ.get("API_HASH", none)
 
     #comma seperated user id of users who are allowed to use
     #ALLOWED_USERS = [x.strip(' ') for x in os.environ.get('ALLOWED_USERS','1098504493').split(',')]
