21831889979031f21913af47c0d5bbcd996c2d91
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-07-17 13:55:23 +0300

    Update muxbot.py

--- muxbot.py
+++ muxbot.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
 
     app = pyrogram.Client(
         session_name='videomerge',
-        bot_token = Config.BOT_TOKEN,
+        session_string = Config.BOT_TOKEN,
         api_id = Config.APP_ID,
         api_hash = Config.API_HASH,
         plugins = plugins
