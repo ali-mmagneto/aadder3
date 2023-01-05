68dc6834b764b6fe3e773b8ff1cc28ebc14298c7
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-01-05 17:50:36 +0300

    Update muxbot.py

--- muxbot.py
+++ muxbot.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     plugins = dict(root='plugins')
 
     app = pyrogram.Client(
-        session_name = 'Muxbot',
+        name = 'Muxbot',
         bot_token = Config.BOT_TOKEN,
         api_id = Config.APP_ID,
         api_hash = Config.API_HASH,
