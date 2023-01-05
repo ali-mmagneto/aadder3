b18ff33aecf7c2eb2b3ec16e93e098846b8cab42
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-01-05 17:47:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,12 +7,6 @@ import pyrogram
 from pyrogram import Client
 
 
-PRE_LOG = os.environ.get("PRE_LOG", "")
-STRING_SESSION = os.environ.get('STRING_SESSION', '')
-
-userbot = Client(name='userbot', api_id=APP_ID, api_hash=API_HASH, session_string=STRING_SESSION, parse_mode=enums.ParseMode.HTML)
-userbot.start()
-print("Userbot Başlatıldı 4 gb yükleme aktif")
 
 class Config:
 
@@ -28,3 +22,12 @@ class Config:
     OWNER_ID = os.environ.get("OWNER_ID", '1276627253') 
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", '-1001157048481')
     
+
+
+    PRE_LOG = os.environ.get("PRE_LOG", "")
+    STRING_SESSION = os.environ.get('STRING_SESSION', '')
+
+    userbot = Client(name='userbot', api_id=APP_ID, api_hash=API_HASH, session_string=STRING_SESSION, parse_mode=enums.ParseMode.HTML)
+    userbot.start()
+    print("Userbot Başlatıldı 4 gb yükleme aktif")
+
