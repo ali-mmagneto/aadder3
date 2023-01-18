acaa4412d97017307580e7bfaf358c6fe44207d7
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2023-01-18 23:15:51 +0300

    Update save_file.py

--- plugins/save_file.py
+++ plugins/save_file.py
@@ -62,7 +62,7 @@ async def save_doc(bot, message, cb=False):
     ext = save_filename.split('.').pop()
     filename = str(round(start_time))+'.'+ext
 
-    if ext in ['aac','mp3']:
+    if ext in ['aac','mp3','m4a']:
         os.rename(Config.DOWNLOAD_DIR+'/'+tg_filename,Config.DOWNLOAD_DIR+'/'+filename)
         db.put_sub(chat_id, filename)
         if db.check_video(chat_id):
