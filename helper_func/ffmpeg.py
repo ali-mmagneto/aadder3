b283eedfc16e925e1fb90a09128033f3dfb5c7b2
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2023-01-20 00:16:34 +0300

    Update ffmpeg.py

--- helper_func/ffmpeg.py
+++ helper_func/ffmpeg.py
@@ -7,7 +7,7 @@ from helper_func.tools import execute, clean_up
 from helper_func.upload import upload_audio, upload_subtitle
 
 async def extract_audio(client, message, data):
-    await message.edit_text("Extracting Stream from file...")
+    await message.edit_text("`Dosyan Ayıklanıyor...`")
 
     dwld_loc = data['location']
     out_loc = data['location'] + ".mp3"
@@ -15,7 +15,7 @@ async def extract_audio(client, message, data):
     if data['name'] == "mp3":
         out, err, rcode, pid = await execute(f"ffmpeg -i '{dwld_loc}' -map 0:{data['map']} -c copy '{out_loc}' -y")
         if rcode != 0:
-            await message.edit_text("**Error Occured. See Logs for more info.**")
+            await message.edit_text("**Hata Oldu.**")
             print(err)
             await clean_up(dwld_loc, out_loc)
             return
@@ -33,7 +33,7 @@ async def extract_audio(client, message, data):
 
 
 async def extract_subtitle(client, message, data):
-    await message.edit_text("Extracting Stream from file")
+    await message.edit_text("`Dosyan Ayıklanıyor`")
 
     dwld_loc = data['location']
     out_loc = data['location'] + ".srt"   
