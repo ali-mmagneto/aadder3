c638517664f351d273dc048ce8d6882c60e7a722
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2023-01-20 00:10:00 +0300

    Update cb_buttons.py

--- plugins/cb_buttons.py
+++ plugins/cb_buttons.py
@@ -1,1 +1,57 @@
-#kalsin bu
+
+from pyrogram import filters
+from pyrogram import Client as trojanz
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+from config import Config
+from script import Script
+
+from helper_func.progress import PRGRS
+from helper_func.tools import clean_up
+from helper_func.download import download_file, DATA
+from helper_funx.ffmpeg import extract_audio, extract_subtitle
+
+
+@trojanz.on_callback_query()
+async def cb_handler(client, query):
+    if query.data == "close": 
+        await query.message.delete()  
+        await query.answer(
+                "Cancelled...",
+                show_alert=True
+            ) 
+
+
+    elif query.data.startswith('audio'):
+        await query.answer()
+        try:
+            stream_type, mapping, keyword = query.data.split('_')
+            data = DATA[keyword][int(mapping)]
+            await extract_audio(client, query.message, data)
+        except:
+            await query.message.edit_text("**Details Not Found**")   
+
+
+    elif query.data.startswith('subtitle'):
+        await query.answer()
+        try:
+            stream_type, mapping, keyword = query.data.split('_')
+            data = DATA[keyword][int(mapping)]
+            await extract_subtitle(client, query.message, data)
+        except:
+            await query.message.edit_text("**Details Not Found**")  
+
+
+    elif query.data.startswith('cancel'):
+        try:
+            query_type, mapping, keyword = query.data.split('_')
+            data = DATA[keyword][int(mapping)] 
+            await clean_up(data['location'])  
+            await query.message.edit_text("**Cancelled...**")
+            await query.answer(
+                "Cancelled...",
+                show_alert=True
+            ) 
+        except:
+            await query.answer() 
+            await query.message.edit_text("**Details Not Found**")        
