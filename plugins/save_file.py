171e0daec06f75a8ccf6015722344791a5bf1c31
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2023-01-20 18:07:59 +0300

    Update save_file.py

--- plugins/save_file.py
+++ plugins/save_file.py
@@ -6,7 +6,6 @@ from config import Config
 from pyrogram import Client, filters
 from helper_func.progress_bar import progress_bar
 from helper_func.dbhelper import Database as Db
-from plugins.forcesub import handle_force_subscribe
 import re
 import json
 import requests
