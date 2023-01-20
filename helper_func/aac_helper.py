179446216d0957413841d8e5e9be9cae71ee64c4
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2023-01-20 19:14:12 +0300

    Update aac_helper.py

--- helper_func/aac_helper.py
+++ helper_func/aac_helper.py
@@ -4,8 +4,8 @@ import shutil
 from plugins.aac import aquee
 from config import Config
 from pyrogram.types import Message
-from functions.ffmpeg import encode, get_codec, get_thumbnail, get_duration, get_width_height
-from functions.progress_bar import progress_bar
+from helper_func.ffmpeg import encode, get_codec, get_thumbnail, get_duration, get_width_height
+from helper_func.progress_bar import progress_bar
 from pyrogram.errors import FloodWait, MessageNotModified, MessageIdInvalid
 from config import Config
 
