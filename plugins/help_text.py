3c616c9a8104c82bb6b8454fc4d0b4be37dac0a2
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2023-01-20 18:06:01 +0300

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -1,12 +1,11 @@
-
-# (c) Shrimadhav uk | @Tellybots_4u
+# :d
 
 import os
 
 import pyrogram
 from config import Config
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from plugins.forcesub import handle_force_subscribe
+
 from script import Script
 from pyrogram import enums
 
