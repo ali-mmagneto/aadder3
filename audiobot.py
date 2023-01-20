2da6de19247ec0f5c7098138176269e610cacb7a
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2023-01-20 22:06:54 +0300

    Update audiobot.py

--- audiobot.py
+++ audiobot.py
@@ -31,26 +31,23 @@ class Bot(Client):
         )
 
     async def start(self):
-        if not os.path.isdir(DOWNLOAD_LOCATION): os.makedirs(DOWNLOAD_LOCATION)
+        if not os.path.isdir(Config.DOWNLOAD_LOCATION): os.makedirs(Config.DOWNLOAD_LOCATION)
         await super().start()
         me = await self.get_me()
         self.username = '@' + me.username
         LOGGER.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}. Premium {me.is_premium}.")
-        if OWNER_ID != 0:
+        if Config.OWNER_ID != 0:
             try:
                 await self.send_message(text="Karanlığın küllerinden yeniden doğdum.",
-                    chat_id=OWNER_ID)
+                    chat_id=Config.OWNER_ID)
             except Exception as t:
                 LOGGER.error(str(t))
 
     async def stop(self, *args):
-        if OWNER_ID != 0:
+        if Config.OWNER_ID != 0:
             texto = f"Son nefesimi verdim.\nÖldüğümde yaşım: {ReadableTime(time.time() - botStartTime)}"
             try:
-                if SEND_LOGS_WHEN_DYING:
-                    await self.send_document(document='log.txt', caption=texto, chat_id=OWNER_ID)
-                else:
-                    await self.send_message(text=texto, chat_id=OWNER_ID)
+                await self.send_document(document='log.txt', caption=texto, chat_id=Config.OWNER_ID)
             except Exception as t:
                 LOGGER.warning(str(t))
         await super().stop()
