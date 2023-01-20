38473cd0063c16c800ef70758483780b703348d7
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2023-01-20 19:16:13 +0300

    Update thumb.py

--- helper_func/thumb.py
+++ helper_func/thumb.py
@@ -4,6 +4,67 @@ import ffmpeg
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 
+
+import os
+import time
+import asyncio
+import ffmpeg
+from subprocess import check_output
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+from config import ENCODE_DIR
+
+def get_codec(filepath, channel="v:0"):
+    output = check_output(
+        [
+            "ffprobe",
+            "-v",
+            "error",
+            "-select_streams",
+            channel,
+            "-show_entries",
+            "stream=codec_name,codec_tag_string",
+            "-of",
+            "default=nokey=1:noprint_wrappers=1",
+            filepath,
+        ]
+    )
+    return output.decode("utf-8").split()
+
+
+async def encode(filepath):
+    path, extension = os.path.splitext(filepath)
+    file_name = os.path.basename(path)
+    encode_dir = os.path.join(
+        ENCODE_DIR,
+        file_name
+    )
+    output_filepath = encode_dir + '.mp4'
+    assert (output_filepath != filepath)
+    if os.path.isfile(output_filepath):
+        print('"{}" Atlanıyor: dosya zaten var'.format(output_filepath))
+    print(filepath)
+
+    # Get the audio and subs channel codec
+    audio_codec = get_codec(filepath, channel='a:0')
+
+    if not audio_codec:
+        audio_opts = '-c:v copy'
+    elif audio_codec[0] in 'aac':
+        audio_opts = '-c:v copy'
+    else:
+        audio_opts = '-c:a aac -c:v copy'
+
+    command = ['ffmpeg', '-y', '-i', filepath]
+    command.extend(audio_opts.split())
+    proc = await asyncio.create_subprocess_exec(
+        *command, output_filepath,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE
+    )
+    await proc.communicate()
+    return output_filepath
+
 def get_thumbnail(in_filename, path, ttl):
     out_filename = os.path.join(path, str(time.time()) + ".jpg")
     open(out_filename, 'a').close()
@@ -32,3 +93,5 @@ def get_width_height(filepath):
       return metadata.get("width"), metadata.get("height")
     else:
       return 1280, 720
+
+
