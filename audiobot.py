73f2b711857696b5ab7e59ce2f2699e076c0afed
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2023-01-20 22:04:47 +0300

    Update audiobot.py

--- audiobot.py
+++ audiobot.py
@@ -8,21 +8,54 @@ db = Db().setup()
 import pyrogram
 from pyrogram import Client
 
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[logging.FileHandler('log.txt'), logging.StreamHandler()],
+    level=logging.INFO)
+LOGGER = logging.getLogger(__name__)
+botStartTime = time.time()
 
-if __name__ == '__main__':
-
-    if not os.path.isdir(Config.DOWNLOAD_DIR):
+if not os.path.isdir(Config.DOWNLOAD_DIR):
         os.mkdir(Config.DOWNLOAD_DIR)
-    if not os.path.isdir(Config.ENCODE_DIR):
+if not os.path.isdir(Config.ENCODE_DIR):
         os.mkdir(Config.ENCODE_DIR)
 
-    plugins = dict(root='plugins')
+class Bot(Client):
 
-    app = Client(
-        name = 'Multibot',
-        bot_token = Config.BOT_TOKEN,
-        api_id = Config.APP_ID,
-        api_hash = Config.API_HASH,
-        plugins = plugins
-    )
-    app.run()
+    def __init__(self):
+        super().__init__(
+            name='multibot',
+            api_id=Config.APP_ID,
+            api_hash=Config.API_HASH,
+            bot_token=Config.BOT_TOKEN,
+            plugins=plugins
+        )
+
+    async def start(self):
+        if not os.path.isdir(DOWNLOAD_LOCATION): os.makedirs(DOWNLOAD_LOCATION)
+        await super().start()
+        me = await self.get_me()
+        self.username = '@' + me.username
+        LOGGER.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}. Premium {me.is_premium}.")
+        if OWNER_ID != 0:
+            try:
+                await self.send_message(text="Karanlığın küllerinden yeniden doğdum.",
+                    chat_id=OWNER_ID)
+            except Exception as t:
+                LOGGER.error(str(t))
+
+    async def stop(self, *args):
+        if OWNER_ID != 0:
+            texto = f"Son nefesimi verdim.\nÖldüğümde yaşım: {ReadableTime(time.time() - botStartTime)}"
+            try:
+                if SEND_LOGS_WHEN_DYING:
+                    await self.send_document(document='log.txt', caption=texto, chat_id=OWNER_ID)
+                else:
+                    await self.send_message(text=texto, chat_id=OWNER_ID)
+            except Exception as t:
+                LOGGER.warning(str(t))
+        await super().stop()
+        LOGGER.info(msg="App Stopped.")
+        exit()
+
+app = Bot()
+app.run()
