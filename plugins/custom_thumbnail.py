fb1dbfce705f20d619772b738dc72540d2207693
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2023-01-20 09:30:43 +0300

    Update custom_thumbnail.py

--- plugins/custom_thumbnail.py
+++ plugins/custom_thumbnail.py
@@ -1,32 +1,56 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from helper_func.dbhelper import Database as Db 
-db = Db()
+from PIL import Image
+from config import DOWNLOAD_DIR, SUDO_USERS
 
-@Client.on_message(filters.command(["setthumb", "set_thumbnail"]) & filters.incoming & filters.reply)
-async def set_thumbnail(c: Client, m: "types.Message"):
-    thumbnail = m.reply_to_message.photo.file_id
-    editable = await m.reply_text("**👀 İşleniyor...**")
-    await db.set_thumbnail(m.from_user.id, thumbnail=thumbnail)
-    await editable.edit("Thumbnail Kaydedildi")
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
-@Client.on_message(filters.private & filters.command(["delthumb", "delete_thumbnail"]))
-async def delete_thumbnail(c: Client, m: "types.Message"):
-    await db.set_thumbnail(m.from_user.id, thumbnail=None)
-    await m.reply_text(
-        "`Thumbnail Silindi..`"
+@Client.on_message(filters.user(SUDO_USERS) & filters.incoming & filters.photo)
+async def save_photo(c, m):
+    v = await m.reply_text("Thumbnail Processing.", True)
+    user_id = str(m.from_user.id)
+    path = os.path.join(
+        DOWNLOAD_DIR,
+        user_id
+    )
+    thumb_image_path = os.path.join(
+        path,
+        user_id + ".jpg"
     )
 
+    downloaded_file_name = await m.download(
+        file_name=thumb_image_path
+    )
+    Image.open(downloaded_file_name).convert(
+        "RGB"
+    ).save(downloaded_file_name)
+    # ref: https://t.me/PyrogramChat/44663
+    img = Image.open(downloaded_file_name)
+    img.save(thumb_image_path, "JPEG")
+    try:
+        await v.edit_text("Thumbnail Saved.")
+    except Exception as e:
+        print(f"#Error {e}")
 
-@Client.on_message(filters.private & filters.command(["showthumb", "show_thumbnail"]))
-async def show_thumbnail(c: Client, m: "types.Message"):
-    thumbnail = await db.get_thumbnail(m.from_user.id)
-    if thumbnail is not None:
-        await c.send_photo(
-            chat_id=m.chat.id,
-            photo=thumbnail,
-            caption=f"**Ayarlı Thumbnail.**",
-            reply_to_message_id=m.id)
-    else:
-        await m.reply_text(text=f"**Thumbnail Bulunamadı.**") 
+
+@Client.on_message(filters.user(SUDO_USERS) & filters.incoming & filters.command(["delthumb"]))
+async def delete_thumbnail(c, m):
+    user_id = str(m.from_user.id)
+    path = os.path.join(
+        DOWNLOAD_DIR,
+        user_id
+    )
+    thumb_image_path = os.path.join(
+        path,
+        user_id + ".jpg"
+    )
+    if os.path.exists(thumb_image_path):
+        os.remove(thumb_image_path)
+    await m.reply_text("Thumbnail Removed.", quote=True)
