41fa67d316663bfa4c9961cdf460cac8d7a3433b
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2023-01-21 11:50:44 +0300

    Update status.py

--- plugins/status.py
+++ plugins/status.py
@@ -2,6 +2,7 @@ import shutil
 import psutil
 import math
 import time
+import logging
 
 from helper_func.progress_bar import humanbytes
 from plugins.save_file import equee
@@ -9,6 +10,24 @@ from plugins.aac import aquee
 from pyrogram import Client, filters
 from config import botStartTime2
 from helper_func.thumb import ReadableTime
+from pyrogram.types import Message
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    handlers=[logging.FileHandler('log.txt'), logging.StreamHandler()],
+                    level=logging.INFO)
+LOGGER = logging.getLogger(__name__)
+
+
+@Client.on_message(filters.command('log'))
+async def log_handler(bot, message):
+    with open('log.txt', 'rb') as f:
+        try:
+            await bot.send_document(document=f,
+                                  file_name=f.name, reply_to_message_id=message.id,
+                                  chat_id=message.chat.id, caption=f.name)
+        except Exception as e:
+            await message.reply_text(str(e))
 
 @Client.on_message(filters.command("status"))
 async def status(bot, message):
