1212e54f548c09486a7c9781164d4289a56b465b
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2023-01-21 13:13:22 +0300

    Update mediainfo.py

--- plugins/mediainfo.py
+++ plugins/mediainfo.py
@@ -1,3 +1,17 @@
+from helper_func.tools import katbin_paste
+from helper_func.tools import get_readable_size, get_readable_bitrate
+
+from pyrogram.types import Message
+from pyrogram import Client, filters
+
+import subprocess
+import requests
+import json
+import re
+import os
+
+
+
 async def telegram_mediainfo(client, message):
     """
     Telegram Dosyasından Mediainfo çıkartır.
