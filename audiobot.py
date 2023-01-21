5223fedc7bd3ba01580fcec7ecce9c97fcb22abf
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-01-21 23:15:37 +0300

    Update audiobot.py

--- audiobot.py
+++ audiobot.py
@@ -26,7 +26,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            name='multibot',
+            name='multivideobot',
             api_id=Config.APP_ID,
             api_hash=Config.API_HASH,
             bot_token=Config.BOT_TOKEN,
