a1f65a1d2faf18a23671bec181298fb5187e8849
Quicksilver81 <86109049+Quicksilver81@users.noreply.github.com>
2023-01-21 13:19:13 +0300

    Update tools.py

--- helper_func/tools.py
+++ helper_func/tools.py
@@ -1,12 +1,34 @@
 import os
 import shlex
 import asyncio
-
+from bs4 import BeautifulSoup
 from typing import Tuple
 
 import string
 import shutil
 import random
+
+async def katbin_paste(text: str) -> str:
+    """
+	paste the text in katb.in website.
+	"""
+
+    katbin_url = "https://katb.in"
+    client = httpx.AsyncClient()
+    response = await client.get(katbin_url)
+
+    soup = BeautifulSoup(response.content, "html.parser")
+    csrf_token = soup.find('input', {"name": "_csrf_token"}).get("value")
+
+    try:
+        paste_post = await client.post(katbin_url, data={"_csrf_token": csrf_token, "paste[content]": text},
+                                       follow_redirects=False)
+        output_url = f"{katbin_url}{paste_post.headers['location']}"
+        await client.aclose()
+        return output_url
+
+    except:
+        return "something went wrong while pasting text in katb.in."
 
 
 def get_readable_time(seconds: int) -> str:
