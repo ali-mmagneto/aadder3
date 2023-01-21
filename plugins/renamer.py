278f4463e31090d57a4c1ce58af64e607a755517
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-01-21 21:38:26 +0300

    Update renamer.py

--- plugins/renamer.py
+++ plugins/renamer.py
@@ -4,8 +4,8 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceRepl
 
 @Client.on_message(filters.command('rename'))
 async def rename(bot, message):
+    chat_id = chat_id
     message = message.reply_to_message
-    msg = await message.reply_text(text="yeni Video ismini yaz", reply_markup=ForceReply(True))
-    msg_reply = msg.get_reply()
-    new_name = msg_reply.text
+    msg = await bot.ask(chat_id, '`Yeni Video İsmini Yazar mısın?`', filters=filters.text) 
+    new_name = msg.text
     print(new_name)
