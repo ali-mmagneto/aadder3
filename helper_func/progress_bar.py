284e878b0ac7f1f37bf89ab581bb8b8ec59c28db
andrevillage <99328594+andrevillage@users.noreply.github.com>
2023-01-23 20:05:28 +0300

    Update progress_bar.py

--- helper_func/progress_bar.py
+++ helper_func/progress_bar.py
@@ -18,8 +18,8 @@ async def progress_bar(current, total, text, message, start):
         ett = TimeFormatter(ett)
 
         progress = "[{0}{1}] \n**İlerleme**: {2}%\n".format(
-            ''.join(["⚫" for i in range(math.floor(percentage / 10))]),
-            ''.join(["⚪" for i in range(10 - math.floor(percentage / 10))]),
+            ''.join(["●" for i in range(math.floor(percentage / 10))]),
+            ''.join(["○" for i in range(10 - math.floor(percentage / 10))]),
             round(percentage, 2))
 
         tmp = progress + "**Indirilen**: {0}/{1}\n**Hız**: `{2}`/s\n**Tahmini Süre**: `{3}`\n".format(
