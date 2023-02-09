f87f2a188dce0ac5e1575b7b926413069de55fba
andrevillage <99328594+andrevillage@users.noreply.github.com>
2023-02-09 14:07:14 +0300

    Update aac_helper.py

--- helper_func/aac_helper.py
+++ helper_func/aac_helper.py
@@ -1,14 +1,40 @@
 import os
 import time
 import shutil
+import re
+import asyncio
 from plugins.aac import aquee
 from config import Config
 from pyrogram.types import Message
-from helper_func.thumb import encode, get_codec, get_thumbnail, get_duration, get_width_height
+from helper_func.thumb import get_codec, get_thumbnail, get_duration, get_width_height
 from helper_func.progress_bar import progress_bar
 from pyrogram.errors import FloodWait, MessageNotModified, MessageIdInvalid
 from config import Config
 
+progress_pattern = re.compile(
+    r'(frame|fps|size|time|bitrate|speed)\s*\=\s*(\S+)'
+)
+
+def parse_progress(line):
+    items = {
+        key: value for key, value in progress_pattern.findall(line)
+    }
+    if not items:
+        return None
+    return items
+
+async def readlines(stream):
+    pattern = re.compile(br'[\r\n]+')
+
+    data = bytearray()
+    while not stream.at_eof():
+        lines = pattern.split(data)
+        data[:] = lines.pop(-1)
+
+        for line in lines:
+            yield line
+
+        data.extend(await stream.read(1024))
 
 async def on_task_complete(bot, message: Message):
     del aquee[0]
@@ -20,6 +46,7 @@ async def add_task(bot, message):
         user_id = str(message.from_user.id)
         c_time = time.time()
         random = str(c_time)
+        caption = message.reply_to_message.caption
 
         if message.reply_to_message.video:
              file_name = message.reply_to_message.video.file_name
@@ -45,7 +72,7 @@ async def add_task(bot, message):
             progress=progress_bar,
             progress_args=("`İndiriliyor...`", msg, c_time))
         await msg.edit("`Video Kodlanıyor...`")
-        new_file = await encode(filepath)
+        new_file = await encode(msg, filepath)
         if new_file:
             await msg.edit("`Yükleniyor`")
             await handle_upload(bot, new_file, message, msg, random)
@@ -95,8 +122,8 @@ async def handle_upload(bot, new_file, message, msg, random):
     caption_str += filename
     caption_str += "</code>"
 
-    if message.caption is not None:
-        caption = message.caption
+    if message.reply_to_message.caption is not None:
+        caption = message.reply_to_message.caption
     else:
         caption = caption_str
     
@@ -163,3 +190,61 @@ async def handle_upload(bot, new_file, message, msg, random):
                os.remove(thumb)
         except:
             pass     
+
+async def encode(msg, filepath):
+    start = time.time()
+    path, extension = os.path.splitext(filepath)
+    file_name = os.path.basename(path)
+    encode_dir = os.path.join(
+        Config.ENCODE_DIR,
+        file_name
+    )
+    output_filepath = encode_dir + '.mp4'
+    assert (output_filepath != filepath)
+    if os.path.isfile(output_filepath):
+        print('"{}" Atlanıyor: dosya zaten var'.format(output_filepath))
+    print(filepath)
+
+    # Get the audio and subs channel codec
+    audio_codec = get_codec(filepath, channel='a:0')
+
+    if not audio_codec:
+        audio_opts = '-c:v copy'
+    elif audio_codec[0] in 'aac':
+        audio_opts = '-c:v copy'
+    else:
+        audio_opts = '-c:a aac -c:v copy'
+
+    command = ['ffmpeg', '-y', '-i', filepath]
+    command.extend(audio_opts.split())
+    proc = await asyncio.create_subprocess_exec(
+        *command, output_filepath,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE
+    )
+    await asyncio.wait([
+            read_stdera(start, msg, proc),
+            proc.wait(),
+        ])
+    await proc.communicate()
+    return output_filepath
+
+
+async def read_stdera(start, msg, proc):
+    async for line in readlines(proc.stderr):
+            line = line.decode('utf-8')
+            progress = parse_progress(line)
+            if progress:
+                #Progress bar logic
+                now = time.time()
+                diff = start-now
+                text = 'İLERLEME\n'
+                text += 'Boyut : {}\n'.format(progress['size'])
+                text += 'Süre : {}\n'.format(progress['time'])
+                text += 'Hız : {}\n'.format(progress['speed'])
+
+                if round(diff % 5)==0:
+                    try:
+                        await msg.edit(text=text)
+                    except Exception as e:
+                        print(e)
