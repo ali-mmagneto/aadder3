55ed3c65b2c8b551c1e9713efb741eb1696ecb74
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-16 16:31:43 +0300

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ botStartTime2 = time.time()
 
 class Config:
 
-    ENCODE_DIR = os.environ.get("ENCODE_DIR", "encodes") 
+    ENCODE_DIR = 'encodes'
     APP_ID = os.environ.get("APP_ID", None)
     API_HASH = os.environ.get("API_HASH", None)
     BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
