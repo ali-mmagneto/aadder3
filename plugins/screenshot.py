960748e329854db432f39649e893ada5781ce324
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-19 13:36:51 +0300

    Update screenshot.py

--- plugins/screenshot.py
+++ plugins/screenshot.py
@@ -75,7 +75,7 @@ async def slowpics_collection(client, message, file_name, path):
             f"Dosya Adı: `{unquote(file_name)}`\n\nScreenshotlar: https://slow.pics/c/{response.text}",
             disable_web_page_preview=True)
         for files in os.listdir(path):
-            await bot.send_photo(
+            await client.send_photo(
                 chat_id=message.chat.id, 
                 photo=files)
         for remover in os.listdir(path):
