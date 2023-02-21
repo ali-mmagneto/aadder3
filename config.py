c6dd20b5f10da97e6c99130e8bed791199352510
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-22 00:59:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class Config:
     API_HASH = os.environ.get("API_HASH", None)
     BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
    
-    
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
     DOWNLOAD_DIR = 'downloads'
     OWNER_ID = os.environ.get("OWNER_ID", '1276627253') 
     
@@ -27,6 +27,10 @@ class Config:
 
     PRE_LOG = os.environ.get("PRE_LOG", "")
     STRING_SESSION = os.environ.get('STRING_SESSION', '')
+    HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
+    MAX_FILE_SIZE = 50000000
+    TG_MAX_FILE_SIZE = 4200000000
+    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "")
 
     userbot = Client(name='userbot', api_id=APP_ID, api_hash=API_HASH, session_string=STRING_SESSION, parse_mode=enums.ParseMode.HTML)
     userbot.start()
