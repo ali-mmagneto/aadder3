c8d3ce0ff1bd9ac9a0c2340a09117dacfce038ef
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-25 12:34:27 +0300

    Update cb_buttons.py

--- plugins/cb_buttons.py
+++ plugins/cb_buttons.py
@@ -55,52 +55,7 @@ LOGGER = logging.getLogger(__name__)
 async def cb_handlers(c: Client, cb: "types.CallbackQuery"):
     user_id = cb.from_user.id
     message = cb.message
-    if cb.data == "home":
-        await cb.answer()
-        await message.edit_text(
-            text=Translation.START_TEXT.format(cb.from_user.mention),
-            reply_markup=Translation.START_BUTTONS,
-            disable_web_page_preview=True
-        )
-    elif cb.data == "help":
-        await cb.answer()
-        await message.edit_text(
-            text=Translation.HELP_TEXT,
-            reply_markup=Translation.HELP_BUTTONS,
-            disable_web_page_preview=True
-        )
-    elif cb.data == "login🔑":
-        await cb.answer()
-        await Login(c, message)
-    elif cb.data == "Settings":
-        await cb.answer()
-        await Settings(message)
-    elif cb.data == "showThumbnail":
-        thumbnail = await db.get_thumbnail(user_id)
-        if not thumbnail:
-            await cb.answer("Herhangi bir thumbnail ayarlamadınız!", show_alert=True)
-        else:
-            await cb.answer()
-            await c.send_photo(message.chat.id, thumbnail, "Ayarlı Thumbnail",
-                               reply_markup=types.InlineKeyboardMarkup([[
-                                   types.InlineKeyboardButton("Sil",
-                                                              callback_data="deleteThumbnail")
-                               ]]))
-    elif cb.data == "deleteThumbnail":
-        await db.set_thumbnail(user_id, None)
-        await cb.answer("Başarıyla silindi.", show_alert=True)
-        await message.delete(True)
-    elif cb.data == "setThumbnail":
-        await cb.answer(Translation.THUMBNAIL_TEXT, show_alert=True)
-    elif cb.data == "triggerGenSS":
-        await cb.answer()
-        generate_ss = await db.get_generate_ss(user_id)
-        if generate_ss:
-            await db.set_generate_ss(user_id, False)
-        else:
-            await db.set_generate_ss(user_id, True)
-        await Settings(message)
-    elif cb.data == "triggerGenSample":
+    if cb.data == "triggerGenSample":
         await cb.answer()
         generate_sample_video = await db.get_generate_sample_video(user_id)
         if generate_sample_video:
@@ -170,8 +125,6 @@ async def cb_handlers(c: Client, cb: "types.CallbackQuery"):
         await message.delete(True)
     elif "|" in cb.data:
         await yt_dlp_call_back(c, cb)
-    elif "=" in cb.data:
-        await ddl_call_back(c, cb)
     else:
         await message.delete(True)
 
