cae23a7f2b27f47074dd6e84ccf0720e4499f3a8
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-25 16:15:26 +0300

    Update streamtapeup.py

--- plugins/streamtapeup.py
+++ plugins/streamtapeup.py
@@ -8,8 +8,9 @@ from pyrogram.enums.parse_mode import ParseMode
 @Client.on_message(filters.command('syukle'))
 async def _main(_, message):
     if message.reply_to_message:
-        await message.reply_text(
+        await bot.send_message(
             "Aşağıdaki Butona Tıkla! ",
+            reply_to_message_id=message.id,
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
