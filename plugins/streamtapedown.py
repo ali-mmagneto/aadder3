331890c660b8f8d313a1fdbaf38a9915a94f9962
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-25 19:52:10 +0300

    Update streamtapedown.py

--- plugins/streamtapedown.py
+++ plugins/streamtapedown.py
@@ -14,7 +14,7 @@ from config import Config
 # https://github.com/viperadnan-git/google-drive-telegram-bot/blob/main/bot/helpers/downloader.py
 def download_file(url, dl_path):
   try:
-    dl = SmartDL(url, dl_path, progress_bar=False)
+    dl = SmartDL(url, dl_path, progress_bar=True)
     dl.start()
     filename = dl.get_dest()
     if '+' in filename:
