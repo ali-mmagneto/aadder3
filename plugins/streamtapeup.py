e73ddf90a0bc4eabe5a4737e421ace0211953319
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-25 15:38:49 +0300

    Update streamtapeup.py

--- plugins/streamtapeup.py
+++ plugins/streamtapeup.py
@@ -9,12 +9,12 @@ from pyrogram.enums.parse_mode import ParseMode
 async def _main(_, message):
     if message.reply_to_message:
         await message.reply_text(
-            "Aşağıdaki Butona Tıkla",
+            "Aşağıdaki Butona Tıkla! ",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [
-                     InlineKeyboardButton("Streamtape'e Yükle", callback_data="uptostreamtape")]
+                     InlineKeyboardButton("Streamtape'e Yükle", callback_data="uptostreamtape")
                 ]
             ),
             quote=True
