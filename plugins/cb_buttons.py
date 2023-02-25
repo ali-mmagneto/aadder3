3d02d24598e954a16e0b827d0e7a8b1fe98e3b2e
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-25 15:57:41 +0300

    Update cb_buttons.py

--- plugins/cb_buttons.py
+++ plugins/cb_buttons.py
@@ -63,7 +63,7 @@ async def cb_handlers(c: Client, cb: "types.CallbackQuery"):
             await db.set_generate_sample_video(user_id, True)
         await Settings(message)
     elif "uptostreamtape" in cb.data:
-        downloadit = cb.message.reply_to_message
+        downloadit = cb.message.reply_to_message.message.reply_to_message
         a = await cb.message.edit("İndiriyorum...", parse_mode=ParseMode.MARKDOWN,
                                     disable_web_page_preview=True)
         dl_loc = Config.DOWNLOAD_DIR + "/" + str(cb.from_user.id) + "/"
