863574c33706376a56d74977894c5a1fb8b54d1b
mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-28 21:27:55 +0300

    Update and rename deneme.py to vpn.py

---
+++ plugins/vpn.py
@@ -0,0 +1,53 @@
+from pyrogram import Client, filters 
+import requests
+from unidecode import unidecode
+import logging
+import os
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    handlers=[logging.FileHandler('log.txt'), logging.StreamHandler()],
+                    level=logging.INFO)
+LOGGER = logging.getLogger(__name__)
+
+@Client.on_message(filters.command('connect'))
+async def connect(client, message):
+    connect = await client.send_message(message.chat.id, "Vpn Bağlanıyor..")
+    try:
+        os.chmod("/etc/openvpn/pass.txt", 600)
+        process = subprocess.Popen(['sudo', '-b', 'openvpn', "--config", "/etc/openvpn/tr-ist.prod.surfshark.com_udp.ovpn", '--auth-user-pass', '/etc/openvpn/pass.txt'], stdout=subprocess.PIPE)
+        while True:
+            output = process.stdout.readline()
+            if output:
+               print(output.strip())
+               if("Initialization Sequence Completed") in str(output):
+                    await connect.edit_text("Başarıyla Bağlandı.")
+                    break
+               elif("Failed or something") in str(output):
+                    await connect.edit_text("Bağlantı başarısız oldu.")
+                    break
+    except Exception as e:
+        await connect.edit(f"HATA: {e}")
+       
+ 
+@Client.on_message(filters.command('disconnect'))
+async def disconnect(client, message):
+    disconnect = await client.send_message(message.chat.id, "Bağlantı kesiliyor..")
+    try:
+        os.system("pgrep openvpn | xargs sudo kill -9")
+        await disconnect.edit_text("Bağlantı kesildi.")
+    except Exception as e:
+        await disconnect.edit(f"HATA: {e}")
+       
+       
+@Client.on_message(filters.command('ip'))
+async def ip(client, message):
+    url = 'http://ipinfo.io/json'
+    response = urlopen(url)
+    data = json.load(response)
+    IP=data['ip']
+    org=data['org']
+    city = data['city']
+    country=data['country']
+    region=data['region']
+    ip = 'IP : {4} \nBölge : {1} \nÜlke : {2} \nŞehir : {3} \nOrg : {0}'.format(org,region,country,city,IP)
+    await client.send_message(message.chat.id, ip)
