af4e0893655db47bdc7a958545de837d7092ea81
mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-28 21:12:42 +0300

    Update deneme.py

--- plugins/deneme.py
+++ plugins/deneme.py
@@ -7,13 +7,18 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
 
+session = requests.Session()
 
 @Client.on_message(filters.command('getlinks'))
 async def linkgetir(bot, message):
     try:
+        Hea={
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36",
+        "content-type": "aplication/json" 
+        }
         text = unidecode(message.text).split()
         link = text[1]
-        veri = requests.get(link)
+        veri = session.get(link, headers=Hea, timeout=15, verify=False)
         veriler = veri.text
         LOGGER.info(veriler)
     except Exception as e:
