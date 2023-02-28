562f014c9ed6636cf98b2f188dc75788d97ee779
mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-28 22:02:38 +0300

    Update vpn.py

--- plugins/vpn.py
+++ plugins/vpn.py
@@ -15,8 +15,8 @@ LOGGER = logging.getLogger(__name__)
 async def connect(client, message):
     connect = await client.send_message(message.chat.id, "Vpn Bağlanıyor..")
     try:
-        os.chmod("/etc/openvpn/pass.txt", 600)
-        process = subprocess.Popen(['sudo', '-b', 'openvpn', "--config", "/etc/openvpn/tr-ist.prod.surfshark.com_udp.ovpn", '--auth-user-pass', '/etc/openvpn/pass.txt'], stdout=subprocess.PIPE)
+        os.chmod("etc/openvpn/pass.txt", 600)
+        process = subprocess.Popen(['sudo', '-b', 'openvpn', "--config", "etc/openvpn/tr-ist.prod.surfshark.com_udp.ovpn", '--auth-user-pass', 'etc/openvpn/pass.txt'], stdout=subprocess.PIPE)
         while True:
             output = process.stdout.readline()
             if output:
