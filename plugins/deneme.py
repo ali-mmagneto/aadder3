a579f6b6de717c1e256c1c94d157a0ee19bc37e7
mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-03-02 20:05:49 +0300

    Update deneme.py

--- plugins/deneme.py
+++ plugins/deneme.py
@@ -3,7 +3,7 @@ import requests
 from unidecode import unidecode
 import logging
 from bs4 import BeautifulSoup
-
+import openai
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     handlers=[logging.FileHandler('log.txt'), logging.StreamHandler()],
                     level=logging.INFO)
@@ -14,28 +14,15 @@ session = requests.Session()
 @Client.on_message(filters.command('getlinks'))
 async def linkgetir(bot, message):
     try:
-        Hea={
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36",
-        "Pragma": "no-cache", 
-        "content-type": "application/json",
-        "lang": "tr-TR",
-        } 
         text = unidecode(message.text).split()
-        link = text[1]
-        sezonsay = text[2]
-        bölümsay = text[3]
-        veri = session.get(link, headers=Hea)
-        veriler = veri.text
-        LOGGER.info(veriler)
-        if '"contentUrl":"' in veriler:
-            m3u8url1 = veriler.split('"contentUrl":"')[1]
-            m3u8url = m3u8url1.split('"')[0]
-            url = m3u8url.replace("\/", "/")
-            await message.reply_text(url)
-            urlurl = session.get(url, headers=Hea)
-            content = urlurl.content
-            soup = BeautifulSoup(content, 'lxml')
-            LOGGER.info(soup)
+        prompt = text[1]
+        response = openai.Image.create(
+            prompt="a white siamese cat",
+            n=1,
+            size="1024x1024"
+            )
+        image_url = response['data'][0]['url']
+        await message.reply_text(image_url)
     except Exception as e:
         await message.reply_text(e)
         
