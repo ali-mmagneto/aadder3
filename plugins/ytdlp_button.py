f2cefc0ab2795fc56260f15479f8a28fee8dbf98
mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-03-21 22:39:31 +0300

    Update ytdlp_button.py

--- plugins/ytdlp_button.py
+++ plugins/ytdlp_button.py
@@ -258,6 +258,9 @@ async def yt_dlp_call_back(bot, update):
     if ".space" in yt_dlp_url:
         command_to_exec.append("--referer")
         command_to_exec.append("https://vidmoly.to/")
+    if ".lat" in yt_dlp_url:
+        command_to_exec.append("--referer")
+        command_to_exec.append("https://vidmoly.to/")
     if "https://upstreamcdn.co" in yt_dlp_url:
         command_to_exec.append("--referer")
         command_to_exec.append("https://upstreamcdn.co")
