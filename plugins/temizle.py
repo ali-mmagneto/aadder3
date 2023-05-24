adbd8ed0e1b582dd817f2db65495ea4e87d635c0
mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-05-24 20:51:22 +0300

    Add files via upload

---
+++ plugins/temizle.py
@@ -0,0 +1,38 @@
+import os
+from pyrogram import Client, filters
+
+@Client.on_message(filters.command('diskd'))
+async def disksil(bot, message):
+    try:
+        silkomut = "rm -rf downloads"
+        try:
+            os.system(silkomut)
+            await message.reply_text("Dosyalar Silindi..")
+        except Exception as e:
+            await message.reply_text("silemedim") 
+        downloadskomut = "mkdir downloads" 
+        try:
+            os.system(downloadskomut)
+            await message.reply_text("downloads klasörü tekrar oluşturuldu") 
+        except Exception as e:
+            await message.reply_text(e)
+    except Exception as e:
+        await message.reply_text(e)
+
+@Client.on_message(filters.command('diske'))
+async def diskesil(bot, message):
+    try:
+        silkomut = "rm -rf encodes"
+        try:
+            os.system(silkomut)
+            await message.reply_text("Dosyalar Silindi..")
+        except Exception as e:
+            await message.reply_text("silemedim") 
+        downloadskomut = "mkdir encodes" 
+        try:
+            os.system(downloadskomut)
+            await message.reply_text("downloads klasörü tekrar oluşturuldu") 
+        except Exception as e:
+            await message.reply_text(e)
+    except Exception as e:
+        await message.reply_text(e)
